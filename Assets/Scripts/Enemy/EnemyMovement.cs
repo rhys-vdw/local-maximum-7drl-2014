e3df36c198fb84093300e8b72f1c85969b63c690
Rhys van der Waerden <rhys.vdw@gmail.com>
2014-03-13 20:34:59 +1100

    Add a couple of things to  start enemy AI. Will return to this after doing some pathfinding.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityObjectRetrieval;
+using System.Collections.Generic;
+using IEnumerator = System.Collections.IEnumerator;
+using System.Linq;
+
+public class EnemyController : MonoBehaviour
+{
+    public float DetectionRadius = 5f;
+    public float DetectionPeriod = 2f;
+
+    Player m_TargetPlayer = null;
+    List<Player> m_PlayersDetected = null;
+
+    Transform m_Transform;
+    LayerMask PlayerMask = ~0;
+
+    void Awake()
+    {
+        m_Transform = transform;
+    }
+
+    void Start()
+    {
+        InvokeRepeating( "DetectPlayers", Random.value * DetectionPeriod, DetectionPeriod );
+    }
+
+    void DetectPlayers()
+    {
+        var playerColliders = Physics.OverlapSphere( m_Transform.position, DetectionRadius, PlayerMask );
+        m_PlayersDetected = playerColliders
+            .Select( c => c.Component<Player>() )
+            .ToList();
+    }
+}
---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityObjectRetrieval;
+using System.Collections.Generic;
+using IEnumerator = System.Collections.IEnumerator;
+
+public class PlayerManager : MonoBehaviour
+{
+    Player[] m_Players = new Player[4];
+
+    public void RegisterPlayer( int number, Player player )
+    {
+        m_Players[ number ] = player;
+    }
+
+    public Player GetPlayer( int number )
+    {
+        return m_Players[ number ];
+    }
+
+    public IEnumerable<Player> Players
+    {
+        get { return m_Players; }
+    }
+}
